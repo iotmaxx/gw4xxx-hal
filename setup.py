dfd91e063730431f42915153c95645f8ad160a3f
Ralf Glaser <glaser@iotmaxx.de>
2021-12-15 11:23:31 +0100

    minimal setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gw4xxx-hal',
+    version='0.1.0',
+    url='https://github.com/iotmaxx/gw4xxx-hal',
+    author='Ralf Glaser',
+    author_email='glaser@iotmaxx.de',
+    description='IoTmaxx gateway series hardware abstraction layer',
+    packages=find_packages(),    
+    install_requires=[],
+)
