a9443c2df41fa7413f8300149a59b0295fda14af
Ralf Glaser <glaser@iotmaxx.de>
2022-08-19 16:21:37 +0200

    GW41HAL-31: add internal io definitions

--- gw4xxx_hal/gw4x00/gw4x00_interfaces.py
+++ gw4xxx_hal/gw4x00/gw4x00_interfaces.py
@@ -20,7 +20,19 @@ gw4x00Interfaces = {
         { "gpiochip": 0, "gpioline": 11 },
         { "gpiochip": 0, "gpioline": 14 },
         { "gpiochip": 0, "gpioline": 15 },
-    ]
+    ],
+    "LEDs" : [
+        "/sys/class/leds/iotmaxx:green:usr1/",
+        "/sys/class/leds/iotmaxx:green:usr2/",
+    ],
+    "user_button":      { "gpiochip": 2, "gpioline": 27 },
+    "sim_enable":       { "gpiochip": 2, "gpioline": 25 },
+    "sim_select":       { "gpiochip": 3, "gpioline": 16 },
+    "CAN_term_en_n":    { "gpiochip": 3, "gpioline": 12 },
+    "GSM_power_ind":    { "gpiochip": 3, "gpioline": 19 },
+    "PBSTAT_IRQ_n":     { "gpiochip": 1, "gpioline":  1 },
+    "ACT8847_IRQ_n":    { "gpiochip": 2, "gpioline": 26 },
+    "RTC_INTA_n":       { "gpiochip": 1, "gpioline": 29 },
 }
 
 gw4x00GpioState = { "high", "low", "tri-state", "input" }
