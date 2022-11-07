bfdace9a05ff459c18db3c9bd7ae12dbdb184eaa
Ralf Glaser <glaser@iotmaxx.de>
2022-11-07 13:33:25 +0100

    add Imei request

--- gw4xxx_hal/gw4xxx/system.py
+++ gw4xxx_hal/gw4xxx/system.py
@@ -1,9 +1,21 @@
+import json
+import subprocess
 
 def getSystemInfo():
     systemInfo = {}
     with open('/sys/bus/soc/devices/soc0/serial_number','r') as f:
         systemInfo['imx7_socid'] = f.readline().strip()
+    systemInfo['IMEI'] = getIMEI()
     return systemInfo
 
-
+def getIMEI():
+    retVal = None
+    try:
+        modemList = json.loads(subprocess.run("mmcli -J -L".split(), stdout=subprocess.PIPE).stdout.decode('utf-8')[:-1])['modem-list']
+        if len(modemList) > 0:         # modem available
+            modemInfo = json.loads(subprocess.run(['mmcli','-J','-m',modemList[0]], stdout=subprocess.PIPE).stdout.decode('utf-8'))
+            retVal = modemInfo['modem']["generic"]["equipment-identifier"]
+    except json.decoder.JSONDecodeError:
+        pass
+    return retVal
 
